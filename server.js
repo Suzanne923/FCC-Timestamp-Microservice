34713e95bd59786918d93f33c390496d523b77d7
Glitch (high-barometer) <none>
2018-02-19 16:38:03 +0000

    🏆✌️ Checkpoint server.js:692490/151

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(express.static(process.cwd() + '/public'));
+app.use(express.static(process.cwd() + '/views'));
+
+app.get('/:str', (req, res) => {
+  const string = req.params.str;
+  console.log(string);
+  res.json({ "unix": 1450137600 })
+});
 
 app.listen(process.env.PORT, () => {
   console.log('Node.js listening ...');
