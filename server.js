004a3d097e51c0063264d7144d01fbf53d846dce
Glitch (high-barometer) <none>
2018-02-19 17:11:06 +0000

    🔋🏄 Checkpoint server.js:692490/271

--- server.js
+++ server.js
@@ -9,14 +9,19 @@ app.use(bodyParser.json());
 app.use(express.static(process.cwd() + '/views'));
 
 function parseDate(iso) {
-  const date = new Date(iso);
-  return date.getMonth + 
+  const format = { 
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric'
+  }
+  const date = new Date(unix);
+  return date.toLocaleString("en-US", format);
 }
 
 app.get('/:str', (req, res) => {
   const string = req.params.str;
-  console.log(string);
-  res.json({ "unix": 1450137600 })
+  const naturalDate = parseDate(string);
+  res.json({ "unix": 1450137600, "natural": naturalDate });
 });
 
 app.listen(process.env.PORT, () => {
