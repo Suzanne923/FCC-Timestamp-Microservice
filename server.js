cd19af00bbed9fdc4e5ed6f74c7a2ead7662870a
Glitch (high-barometer) <none>
2018-02-19 16:27:50 +0000

    👅🏆 Checkpoint package.json:692490/24 server.js:692490/173

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
 const express = require('express');
-const http = require('http');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
-if (!process.env.DISABLE_XORIGIN) {
-  app.use((req, res, next) => {
-    var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
-    var origin = req.headers.origin || '*';
-    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
-         console.log(origin);
-         res.setHeader('Access-Control-Allow-Origin', origin);
-         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    }
-    next();
-  });
-}
+app.use(cors());
+app.use(bodyParser.json());
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.static(process.cwd() + '/public'));
 
-const server = http.createServer(app);
-server.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log('Node.js listening ...');
 });
 
